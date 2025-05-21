42c74fadba782dd089525635852f4a05622151b7
Nitesh Singh <niteshsingh.se@gmail.com>
2025-05-21 12:33:13 +0530

    added type in theme provider

--- utils/ThemeProvider.tsx
+++ utils/ThemeProvider.tsx
@@ -1,6 +1,12 @@
 "use client";
-// import {ThemeProvider as NextThemesProvider} from "next-themes";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-export default function ThemeProvider({children, ...props}) {
+
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes";
+import { ReactNode } from "react";
+
+interface Props extends ThemeProviderProps {
+  children: ReactNode;
+}
+
+export default function ThemeProvider({ children, ...props }: Props) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
